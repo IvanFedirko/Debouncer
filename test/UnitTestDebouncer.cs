13edc165633940cc6e2f20918c940995c05439ed
Ivan <fedirko.ivan@gmail.com>
2024-07-19 10:06:49 +0700

    Adding tests

--- test/UnitTestDebouncer.cs
+++ test/UnitTestDebouncer.cs
@@ -14,17 +14,36 @@ public class UnitTestDebouncer
     [TestMethod]
     public void TestHandlingBtnPressing()
     {
-        Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
         //Arrange
-        int testVal = 0;
-        ButtonDebouncer<int> debouncer = new ButtonDebouncer<int>((v) => testVal = v, TimeSpan.FromMilliseconds(100));
+        int testValInt = 0;
+        ButtonDebouncer<int> debouncerInt = new ButtonDebouncer<int>((v) => testValInt = v, TimeSpan.FromMilliseconds(100));
 
         //Act
-        for (int i = 0; i < 10; i++)   { debouncer.HandlingBtnPressing(i); System.Threading.Thread.Sleep(1); }
-        System.Threading.Thread.Sleep(1000); 
+        for (int i = 0; i < 10; i++) { debouncerInt.HandlingBtnPressing(i);  }
+        System.Threading.Thread.Sleep(1000);
 
         //Assert
-        Assert.AreEqual(9, testVal);
+        Assert.AreEqual(9, testValInt);
+
+
+        //Arrange
+        bool testValBool = false;
+        ButtonDebouncer<bool> debouncerBool = new ButtonDebouncer<bool>((v) => testValBool = v, TimeSpan.FromMilliseconds(100));
+
+        //Act
+        debouncerBool.HandlingBtnPressing(true); 
+        debouncerBool.HandlingBtnPressing(false); 
+        System.Threading.Thread.Sleep(1000);
+
+        //Assert
+        Assert.AreEqual(false, testValBool);
+
+
+        //Act
+        debouncerBool.HandlingBtnPressing(true); 
+        //Assert
+        Assert.AreEqual(true, testValBool);
 
     }
 }
